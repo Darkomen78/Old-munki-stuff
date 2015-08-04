f3ab2034c96f71d7a0db4c61c4bcc058c6ebc894
Darkomen <darkomen78@gmail.com>
2015-08-04 16:45:19 +0200

    Fix manifest settings

--- Munki_packagr.sh
+++ Munki_packagr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 1.2 by Sylvain La Gravière
+# Version 1.2.1 by Sylvain La Gravière
 # Twitter : @darkomen78
 # Mail : darkomen@me.com
 
@@ -113,11 +113,10 @@ if [ "${checkboxes[1]}" = "1" ]; then
       RESPONSE=`$POPUP $RUNMODE --button1 "Ok" $OTHEROPTS  --icon $ICON --title "${TITLE}" --text "${TEXT}" --label "$TEXTB"`
       MANIFEST=`echo $RESPONSE | sed 's/Ok//g' | sed 's/ //g'`
       echo "• Le manifest est le fichier "mymanifest" sur le serveur mymunki" >> "$ROOTDIR"/Munki2_source/intro.txt
-      sed -i .temp "s/mymanifest/$MANIFEST/g" "$ROOTDIR"/Munki2_source/intro.txt
       sed -i .temp "s/mymanifest/$MANIFEST/g" "$ROOTDIR"/Munki2_source/CLIENT.configure
 else
       echo "• Il n'y a pas de manifest par défaut pour ce client" >> "$ROOTDIR"/Munki2_source/intro.txt
-      MANIFEST="no_manifest"
+      sed -i .temp "s/mymanifest//g" "$ROOTDIR"/Munki2_source/CLIENT.configure
 fi
 
 if [ "${checkboxes[2]}" = "1" ]; then
