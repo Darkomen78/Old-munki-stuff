b9de6c56429a4f8f9cb7250de17f7425dde64760
Darkomen <darkomen78@gmail.com>
2015-08-14 14:30:12 +0200

    test xml choice

--- Munki_packagr.sh
+++ Munki_packagr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 1.2.1b by Sylvain La Gravière
+# Version 1.3 by Sylvain La Gravière
 # Twitter : @darkomen78
 # Mail : darkomen@me.com
 
@@ -62,13 +62,11 @@ if [ -d "$ROOTDIR"/Munki2_source ]; then
             rm -R "$ROOTDIR"/Munki2_source
             echo "Download latest version of pkg source..."
 	      curl -O -s -L "$GITSRC"Munki2prepkg.zip
-            unzip "Munki2prepkg.zip" &> /dev/null && rm "Munki2prepkg.zip" && mv Munki2prepkg/* $ROOTDIR/ && rm -R Munki2prepkg            
+            unzip -q "Munki2prepkg.zip" && rm "Munki2prepkg.zip"         
             echo "Download latest version of Munki..."
             curl -s -L "$MUNKISRC" -o "$ROOTDIR"/Munki2_source/munkitools2.pkg && pkgutil --expand "$ROOTDIR"/Munki2_source/munkitools2.pkg "$ROOTDIR"/Munki2_source/src
             MUNKIVER=$(ls -la "$ROOTDIR"/Munki2_source/src/ | grep core | sed 's/.*-//' | sed 's/.pkg//')
-            mv "$ROOTDIR"/Munki2_source/src/*app*.pkg "$ROOTDIR"/Munki2_source/src/app.pkg
-            mv "$ROOTDIR"/Munki2_source/src/*core*.pkg "$ROOTDIR"/Munki2_source/src/core.pkg
-            mv "$ROOTDIR"/Munki2_source/src/*launchd*.pkg "$ROOTDIR"/Munki2_source/src/launchd.pkg
+            rm -Rf "$ROOTDIR"/Munki2_source/src
             echo "...update version on pkgproj file to" $MUNKIVER
             packagesutil --file Munki2.pkgproj set package-1 version $MUNKIVER
       else
@@ -77,13 +75,11 @@ if [ -d "$ROOTDIR"/Munki2_source ]; then
 else
       echo "Download latest version of pkg source..."
       curl -O -s -L "$GITSRC"Munki2prepkg.zip
-      unzip "Munki2prepkg.zip" &> /dev/null && rm "Munki2prepkg.zip" && mv Munki2prepkg/* $ROOTDIR/ && rm -R Munki2prepkg
+      unzip -q "Munki2prepkg.zip" && rm "Munki2prepkg.zip"
       echo "Download latest version of Munki..."
       curl -s -L "$MUNKISRC" -o "$ROOTDIR"/Munki2_source/munkitools2.pkg && pkgutil --expand "$ROOTDIR"/Munki2_source/munkitools2.pkg "$ROOTDIR"/Munki2_source/src
       MUNKIVER=$(ls -la "$ROOTDIR"/Munki2_source/src/ | grep core | sed 's/.*-//' | sed 's/.pkg//')
-      mv "$ROOTDIR"/Munki2_source/src/*app*.pkg "$ROOTDIR"/Munki2_source/src/app.pkg
-      mv "$ROOTDIR"/Munki2_source/src/*core*.pkg "$ROOTDIR"/Munki2_source/src/core.pkg
-      mv "$ROOTDIR"/Munki2_source/src/*launchd*.pkg "$ROOTDIR"/Munki2_source/src/launchd.pkg
+      rm -Rf "$ROOTDIR"/Munki2_source/src
       echo "...update version on pkgproj file to" $MUNKIVER
       packagesutil --file Munki2.pkgproj set package-1 version $MUNKIVER
 fi
